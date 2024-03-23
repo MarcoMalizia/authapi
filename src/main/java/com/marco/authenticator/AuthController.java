6dfc1f42f40055d4277b899e99fecf021ed187a0
Marco Malizia <marco.malizia@hotmail.it>
2024-03-24 00:15:06 +0100

    new split

--- src/main/java/com/marco/authenticator/AuthController.java
+++ src/main/java/com/marco/authenticator/AuthController.java
@@ -20,11 +20,15 @@ public class AuthController {
 		System.out.println(data);
 		URLDecoder.decode(data, "UTF-8");
 		System.out.println(data);
-		String[] dataArr = data.split("&");
+		/*String[] dataArr = data.split("&");
 		String[] emailArr = dataArr[0].split("=");
 		String email = emailArr[1].replace("%40", "@");
 		String[] passArr = dataArr[1].split("=");
 		String pass = passArr[1];
+		*/
+		String[] dataArr = data.split("-----");
+		String email = dataArr[0];
+		String pass = dataArr[1];
 		final String apiUrl = "https://authenticator.happyforest-825d7b85.northeurope.azurecontainerapps.io:8080/api/users/"+email;
 		RestTemplate restTemplate = new RestTemplate();
 		String results = restTemplate.getForObject(apiUrl, String.class);
